759d9d769783e7fa514ebc6ad371378e8b09f058
RamiroRojasBociga <111482131+RamiroRojasBociga@users.noreply.github.com>
2024-05-10 12:44:30 -0500

    Se agrega vista para vendedor y se activa la pestaña contactos

---
+++ src/main/java/org/example/redvendedores/controlador/ContactosViewControler.java
@@ -0,0 +1,21 @@
+package org.example.redvendedores.controlador;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class ContactosViewControler {
+
+    @FXML
+    private Button btnAgregarContacto;
+
+    @FXML
+    private ListView<?> listViewContactos;
+
+    @FXML
+    void agregarContacto(ActionEvent event) {
+
+    }
+
+}
